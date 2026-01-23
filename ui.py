90c10ec72f5cb1b5a1e2d35e62431a6ca39ff562
Quentin Lurkin <qlurkin@gmail.com>
2026-01-23 12:17:56 +0100

    Better Edit Modal

--- ui.py
+++ ui.py
@@ -1,6 +1,6 @@
 from typing import Callable
 from textual.app import App, ComposeResult
-from textual.containers import HorizontalGroup, Grid
+from textual.containers import HorizontalGroup, Grid, Container
 from textual.widgets import Button, Footer, Header, DataTable, Label, Input
 from textual.widgets.data_table import RowKey, ColumnKey, CellType
 from textual.reactive import reactive
@@ -24,45 +24,65 @@ class Sheet(DataTable):
         value = "" if event.value is None else str(event.value)
         self.app.push_screen(
             CellInputScreen(
-                event.cell_key.row_key, event.cell_key.column_key, value, type
+                event.cell_key.row_key, event.cell_key.column_key, value, type, "end"
             )
         )
 
 
 class CellInputScreen(ModalScreen):
+    BINDINGS = [
+        ("escape", "cancel", "Cancel"),
+    ]
+
     def __init__(
         self,
         row: RowKey,
         column: ColumnKey,
         value: str,
         type: Callable,
+        cursor: str = "replace",
     ):
         super().__init__()
         self.value = value
         self.row = row
         self.column = column
         self.type = type
+        self.cursor = cursor
 
     def compose(self) -> ComposeResult:
         yield Grid(
-            Label(f"{self.column.value} for {self.row.value}"),
+            Container(),
+            Label(f"{self.column.value}"),
+            Label(f"{self.row.value}"),
             Input(self.value, placeholder=self.value),
             Button("Ok", variant="success", id="ok"),
             Button("Cancel", variant="primary", id="cancel"),
-            id="dialog",
         )
+
+    def on_mount(self):
+        input = self.query_one(Input)
+        if self.cursor == "start":
+            input.cursor_position = 0
+        if self.cursor == "end":
+            input.cursor_position = len(input.value)
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         if event.button.id == "ok":
-            row = self.row.value
-            column = self.column.value
-            assert isinstance(row, str)
-            assert isinstance(column, str)
-            value = self.type(self.query_one(Input).value)
-            self.post_message(DocumentUpdate(row, column, value))
-            self.app.pop_screen()
+            self.on_input_submitted()
         else:
-            self.app.pop_screen()
+            self.action_cancel()
+
+    def on_input_submitted(self):
+        row = self.row.value
+        column = self.column.value
+        assert isinstance(row, str)
+        assert isinstance(column, str)
+        value = self.type(self.query_one(Input).value)
+        self.post_message(DocumentUpdate(row, column, value))
+        self.app.pop_screen()
+
+    def action_cancel(self):
+        self.app.pop_screen()
 
 
 class UI(App):
@@ -91,7 +111,11 @@ class UI(App):
 
     def on_document_update(self, event: DocumentUpdate):
         self.doc[event.row, event.column] = event.value
+        coord = self.table.cursor_coordinate
         self.render_table()
+        self.table.cursor_type = "cell"
+        self.table.focus()
+        self.table.cursor_coordinate = coord
 
     def render_table(self):
         self.table.clear(columns=True)
