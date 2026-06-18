05af3b5f531b74620f6d580e5694dcccc679eb2a
Quentin Lurkin <qlurkin@gmail.com>
2026-06-18 09:48:11 +0200

    startup

--- src/grades/__main__.py
+++ src/grades/__main__.py
@@ -1,2 +1,9 @@
+from .document import Document
+from .ui import UI
+from datetime import datetime
+
 if __name__ == "__main__":
-    pass
+    doc = Document(
+        title="No Title", course="No Course", code="xxxxxx", date=datetime.now()
+    )
+    UI(doc).run()
